0ee21d419b79ca90c530bc72cefd3fddc4a82046
inabakumori <userloggedout@gmail.com>
2023-02-19 17:00:01 -0500

    CMake file added for compiling on windows

---
+++ Tetris_Visual_Studio.h
@@ -0,0 +1,8 @@
+﻿// Tetris_Visual_Studio.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
